27673c327fe414a75b2bb193aa88d8c9b9a0e83d
Kense <ningkense@alumni.wfu.edu>
2019-09-27 16:26:28 +0200

    completed functions for hill-climb, need to graph and add user input

--- metaheuristics/tp_1/nk_landscape.py
+++ metaheuristics/tp_1/nk_landscape.py
@@ -69,7 +69,7 @@ def deterministic_hill (point, kVal, lookup):
     '''
     current = point[:] #sets current position to the point
     neighbors = calculate_neighbors(current)
-    neighbor_fits = [evaluate_fitness(x, kVal, lookup) for x in neighbors] #creates a list of fits
+    neighbor_fits = [evaluate_fitness(x, kVal, lookup) for x in neighbors] #creates a list of neighbor fits
     best = neighbors[neighbor_fits.index(max(neighbor_fits))] #this is the best neighbor
     if evaluate_fitness(current, kVal, lookup) >= evaluate_fitness(best, kVal, lookup): #if our fit is the best fit
         return current
@@ -77,15 +77,63 @@ def deterministic_hill (point, kVal, lookup):
         print(best) #prints the current best for reference
         return deterministic_hill(best, kVal, lookup) #iterate until best is current
 
+def roulette (probs):
+    '''
+    takes a list of probabilities and the probability sum to generate a choice index
+    '''
+    a_probs = [0]*len(probs)
+    for i in range(0, len(probs)):
+        a_probs[i] = probs[i] #copies values
+    for j in range(1, len(a_probs)):
+        a_probs[i] += a_probs[i-1]
+    sumP = a_probs[len(a_probs)-1]
+    roll = random.uniform(0, sumP) #generate a random roll between 0 and the end of the board
+    for p in range(0, len(a_probs)-1): #iterate through the board
+        if (roll >= a_probs[p]) and (roll <= a_probs[p+1]):
+            return a_probs.index(a_probs[p+1]) #found the index
+        else:
+            return len(a_probs)-1 #index is last slot
 
-def probabilistic_hill (point, kVal, lookup):
+
+def best_ever (neighbors_fit, best_fit):
+    '''
+    determines if best is still better than neighbor
+    '''
+    if max(neighbors_fit) > best_fit: #strictly better
+        return True
+    else:
+        return False
+
+def probabilistic_hill (point, kVal, lookup, cycle, best_fit):
     '''
     given a point, return the point with the best fitness according to probabilistic hill-climbing.
+    runs is the max number of times you wish to run the algorithm.
     '''
-    pass
+    current = point[:] #sets current position to the point
+    #best_fit = evaluate_fitness(current, kVal, lookup) #the best fit is our current fit
+
+    #some number of runs
+    neighbors = calculate_neighbors(current)
+    neighbor_fits = [evaluate_fitness(x, kVal, lookup) for x in neighbors] #create a list of neighbor fits
+    if(best_ever(neighbor_fits, best_fit)):
+        if cycle <= 0: #stops the runs
+            return choice
+        choice = neighbors[neighbor_fits.index(max(neighbor_fits))] #choose the best ever
+        best_fit = evaluate_fitness(choice, kVal, lookup) #mark the best fit
+        print(choice)
+        return probabilistic_hill(choice, kVal, lookup, cycle-1, best_fit)
+    sum_fits = sum(neighbor_fits) #calculate the total summation of neighbor fits for the probability
+    neighbor_prob = [(y/sum_fits) for y in neighbor_fits] #calculates the probability of each neighbor
+    choice = neighbors[roulette(neighbor_prob)] #chooses the best neighbor
+
+    if cycle <= 0: #stops the run
+        return choice
+    else:
+        print(choice)
+        return probabilistic_hill(choice, kVal, lookup, cycle-1, best_fit) #continue
+
 
 if __name__ == "__main__":
-    a = start_location(21)
-    print(a)
-    best = deterministic_hill(a, 0, {"0":0, "1":1})
-    print(best)
+    f0 = {"0":2, "1":1}
+    f1 = {"00":2, "01":3, "10":2, "11":0}
+    f2 = {"000":0, "001":1, "010":1, "011":0, "100":2, "101":0, "110":0, "111":0}
