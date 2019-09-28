bbe47cc09df47fb9a119b8e73b2334edc6b4948b
Kense <ningkense@alumni.wfu.edu>
2019-09-28 17:20:26 +0200

    Finished up metaheuristics functions

--- metaheuristics/tp_1/nk_landscape.py
+++ metaheuristics/tp_1/nk_landscape.py
@@ -74,7 +74,6 @@ def deterministic_hill (point, kVal, lookup):
     if evaluate_fitness(current, kVal, lookup) >= evaluate_fitness(best, kVal, lookup): #if our fit is the best fit
         return current
     else:
-        print(best) #prints the current best for reference
         return deterministic_hill(best, kVal, lookup) #iterate until best is current
 
 def roulette (probs):
@@ -120,7 +119,6 @@ def probabilistic_hill (point, kVal, lookup, cycle, best_fit):
             return choice
         choice = neighbors[neighbor_fits.index(max(neighbor_fits))] #choose the best ever
         best_fit = evaluate_fitness(choice, kVal, lookup) #mark the best fit
-        print(choice)
         return probabilistic_hill(choice, kVal, lookup, cycle-1, best_fit)
     sum_fits = sum(neighbor_fits) #calculate the total summation of neighbor fits for the probability
     neighbor_prob = [(y/sum_fits) for y in neighbor_fits] #calculates the probability of each neighbor
@@ -129,11 +127,102 @@ def probabilistic_hill (point, kVal, lookup, cycle, best_fit):
     if cycle <= 0: #stops the run
         return choice
     else:
-        print(choice)
         return probabilistic_hill(choice, kVal, lookup, cycle-1, best_fit) #continue
 
+def run_tp (N=21, K=0, method=0):
+    '''
+    function to run the tp with user-defined parameters.
+    defaults to N = 21, K = 0, and a deterministic hill-climbing method.
+    N is the number of bits per point.
+    K is the neighbors taken into account, corresponding to fk.
+    method is 0 for deterministic, 1 for probabilistic.
+    returns the bit sequence solution as a list of int.
+    '''
+    try:
+        #set up the fk values according to the tp
+        f0 = {"0":2, "1":1}
+        f1 = {"00":2, "01":3, "10":2, "11":0}
+        f2 = {"000":0, "001":1, "010":1, "011":0, "100":2, "101":0, "110":0, "111":0}
+        f_list = []
+        f_list.append(f0)
+        f_list.append(f1)
+        f_list.append(f2)
+        f = f_list[K] #choose the proper dictionary corresponding to fk.
+
+        a = start_location(N) #generate the point
+        if(method):
+            a = probabilistic_hill(a, K, f, 100, evaluate_fitness(a, K, f))
+        else:
+            a = deterministic_hill(a, K, f)
+
+        print(s_bits(a), ": ", evaluate_fitness(a, K, f)) #"return the sequence x maximizing the fitness, as well as the corresponding value of the fitness, in the form x: F(x)"
+        return a
+    except:
+        print("Error. Check your N, K, and method values again.")
+        exit()
+
+def calculate_hamming (a, b):
+    '''
+    calculate the hamming distance between two solution points
+    takes two sequence of bits in list form.
+    '''
+    distance = 0
+    for i in range(0, len(a)):
+        if a[i] != b[i]:
+            distance +=1
+    return distance
+
+def run_trial(N, K, method):
+
+    trial = [0]*50
+    a = run_tp(N, K, method) #preliminary run
+    trial[0] = a
+    for i in range(1, 50): #the other 49 runs
+        a = run_tp(N, K, method)
+        trial[i] = a #store the solution
+    hist = [0]*len(trial)
+    for j in range(0, len(trial)):
+        for d in range(j, len(trial)): #for comparing agaist the rest
+            hist[j] += calculate_hamming(trial[j], trial[d]) #accumulate the hamming distances
+    res = {}
+    for c in range (0, len(trial)):
+        res[s_bits(trial[c])] = hist[c] #use a dictionary to make a nice bits -> stability representation
+    return res
 
 if __name__ == "__main__":
-    f0 = {"0":2, "1":1}
-    f1 = {"00":2, "01":3, "10":2, "11":0}
-    f2 = {"000":0, "001":1, "010":1, "011":0, "100":2, "101":0, "110":0, "111":0}
+
+    #trial 1-1
+    t1_1 = run_trial(21, 0, 0) #K=0, deterministic
+    #trial 1-2
+    t1_2 = run_trial(21, 1, 0) #K=1, deterministic
+    #trial 1-3
+    t1_3 = run_trial(21, 2, 0) #K=2, deterministic
+
+    #trial 2-1
+    t2_1 = run_trial(21, 0, 1) #K=0, probabilistic
+    #trial2-2
+    t2_2 = run_trial(21, 1, 1) #K=1, probabilistic
+    #trial2-3
+    t2_3 = run_trial(21, 2, 1) #K=2, probabilistic
+
+    import matplotlib.pyplot as plt #plotting library for python
+    #%matplotlib inline #for jupyter notebook display purposes
+
+    plt.figure(figsize=(9,9))
+
+    plt.subplot(231).title.set_text('K=0, Deterministic')
+    plt.bar(list(t1_1.keys()), t1_1.values(), color='b')
+    plt.subplot(232).title.set_text('K=1, Deterministic')
+    plt.bar(list(t1_2.keys()), t1_2.values(), color='b')
+    plt.subplot(233).title.set_text('K=2, Deterministic')
+    plt.bar(list(t1_3.keys()), t1_3.values(), color='b')
+
+    plt.subplot(234).title.set_text('K=0, Probabilistic')
+    plt.bar(list(t2_1.keys()), t2_1.values(), color='g')
+    plt.subplot(235).title.set_text('K=1, Probabilistic')
+    plt.bar(list(t2_2.keys()), t2_2.values(), color='g')
+    plt.subplot(236).title.set_text('K=2, Probabilistic')
+    plt.bar(list(t2_3.keys()), t2_3.values(), color='g')
+
+    plt.suptitle("TP1 Stability Histograms")
+    plt.show()
