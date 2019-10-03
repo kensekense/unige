d4545230be5604ac8b4d7085fa352ff9d24c8bc7
Kense <ningkense@alumni.wfu.edu>
2019-10-03 13:04:39 +0200

    morning of 3 oct work

--- metaheuristics/tp_2/qap.py
+++ metaheuristics/tp_2/qap.py
@@ -9,23 +9,35 @@ with 1-indexes in order to not get confused in calculations.
 '''
 
 import random
+import numpy as np
+import math
 
 class QuadraticAssignment:
 
-    def __init__ (self, N):
+    def __init__ (self, N, D, W):
         '''
         Initial assignment randomly generates a solution matrix D and W, where D is the distance matrix
         and W is the weight matrix, and N is the number of locations (and facilities).
         '''
-        #generate initial solution
+        self.N = N
+        self.D = D
+        self.W = W
+        #generate initial solution #TODO: this is not trivial, since you have to scramble an array
+        #we would have to scramble the array using temporary values and make sure we don't mess up the symmetry
+        #maybe we can find a way to scramble it on one diagonal and match the symmetry after?
+        #so we can technically read in only the diagonal, make the rest 0s, then scramble the values, and make
+        #it symmetric using np.maximum(a, a.transpose()) because that takes the higher values (our values >0)
+        #to do a triangle array, we do a[i][j] and 2x for loop i, j, with j = i + 1
+
         #create empty tabu list (tabu list should be a matrix of NxN, initialized with 0s)
+        #note that tabu list has a short and long term memory mechanism that we should separate
 
     def generate_neighbor(self):
         '''
         defining neighborhood placeholder.
         '''
         #neighborhood is given by the classical 2-exchange which amounts to swapping two locations
-        #
+        #the formula given is better understood as, partial = 2*np.sum((w[i][j] - w[p][q])*(d1[i][j]-d2[i][j]))
 
     def tabu_search(self):
         '''
@@ -39,4 +51,7 @@ class QuadraticAssignment:
 
 
 if __name__ == "__main__":
+
+    #read the .dat file here and fill the arrays
+
     return 0
