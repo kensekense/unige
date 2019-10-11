1f6f21d0a719233d59836cae7746d44dcc651a91
Kense <ningkense@alumni.wfu.edu>
2019-10-11 11:44:25 +0200

    tp2 qap -short term mem ups

--- metaheuristics/tp_2/qap.py
+++ metaheuristics/tp_2/qap.py
@@ -5,10 +5,13 @@
 import random
 import numpy as np
 import math
+import sys
+
+sys.setrecursionlimit(20000) #WARNING: python recursion issue
 
 class QuadraticAssignment:
 
-    def __init__ (self, data):
+    def __init__ (self, data, stop_condo):
         '''
         Initial assignment randomly generates a solution matrix D and W, where D is the distance matrix
         and W is the weight matrix, and N is the number of locations (and facilities).
@@ -39,6 +42,9 @@ class QuadraticAssignment:
         self.tabu_list = np.zeros((self.N, self.N))
         #note that tabu list has a short and long term memory mechanism that we should separate
 
+        #need a stop condition on iterations
+        self.tmax = stop_condo
+
     def calculate_fitness(self, N, D, W, configuration):
         sum = 0
         #triangle is 2x for loop i,j with j = i+1
@@ -55,31 +61,71 @@ class QuadraticAssignment:
 
         return partial  * 2
 
-    def generate_neighbor(self):
-        '''
-        defining neighborhood placeholder.
-        '''
-        #neighborhood is given by the classical 2-exchange which amounts to swapping two locations
-        #the formula given is better understood as, partial = 2*np.sum((w[i][j] - w[p][q])*(d1[i][j]-d2[i][j]))
-        pass
+    def add_to_tabu(self, N, iter, tenure, taboo, swap):
+        t = tenure*N #taboo tenure l, defined by the notes
+        if (tenure==1):
+            t = 1
+        #edit the taboo value
+        tab = t + random.randint(0,N)
+        taboo[swap[0][0]][swap[0][1]] = tab  #loc 0 -> fac 0
+        taboo[swap[1][0]][swap[1][1]] = tab #loc 1 -> fac 1
+        taboo[swap[0][0]][swap[1][1]] = tab #loc 0 -> fac 1
+        taboo[swap[1][0]][swap[0][1]] = tab #loc 1 -> fac 0
 
-    def tabu_search(self):
+        return taboo
+
+    def suitable_selection(self, N, D, W, configuration):
+        for i in range(random.randint(0,len(configuration)-1),len(configuration)):
+            for j in range(0, len(configuration)):
+                pot_swap = [(configuration[i][0],configuration[i][1]),(configuration[j][0],configuration[j][1])]
+                advantage = self.partial_sum(N,D,W,pot_swap)
+                if advantage < 0:
+                    return pot_swap
+
+    def tabu_search(self, N, D, W, iter, tenure, taboo, configuration):
         '''
         tabu search algorithm placeholder.
         '''
-        #choose the best non-tabu neighbor
-        #update the best solution seen so far
-        #check whether end condition is met
-        #update tabu list with movement x0 -> x1 or generate_neighbor
-        pass
+        advantage = 0
 
+        '''while(advantage >= best): #need to find a good roll
+            roll1 = 0
+            roll2 = 0
+            while(roll1 == roll2):
+                roll1 = random.randint(0,len(configuration)-1) #each time makes two rolls
+                roll2 = random.randint(0,len(configuration)-1)
+
+            #randomly choose a potential swap
+            pot_swap = [(configuration[roll1][0],configuration[roll1][1]),(configuration[roll2][0],configuration[roll2][1])]
+            #calculate the advantage we get from this swap
+            advantage = self.partial_sum(N,D,W,pot_swap)'''
+        pot_swap = self.suitable_selection(N,D,W, configuration)
+
+        #choose the best non-tabu neighbor, first run is okay
+        if(iter == 0 or (taboo[pot_swap[0][0]][pot_swap[1][1]] != taboo[pot_swap[1][0]][pot_swap[0][1]])): #if a valid placement
+            if(iter == 0 or (iter > taboo[pot_swap[0][0]][pot_swap[1][1]] and iter > taboo[pot_swap[1][0]][pot_swap[0][1]])): #from loc 0-> fac 1, and loc 1-> fac 0
+                #swap is accepted
+                taboo = self.add_to_tabu(N, iter, tenure, taboo, pot_swap) #adjust tabu list
+
+                #swaps fac from roll1 with fac from roll2 while maintaining respective loc in config
+                (tloc_a, tfac_a) = configuration[pot_swap[0][0]]
+                (tloc_b, tfac_b) = configuration[pot_swap[1][0]]
+                configuration[pot_swap[0][0]] = (tloc_a, tfac_b)
+                configuration[pot_swap[1][0]] = (tloc_b, tfac_a)
+
+
+        #check whether end condition is met
+        if(iter > self.tmax):
+            return configuration
+
+        return self.tabu_search(N, D, W, iter+1, tenure, taboo, configuration)
 
 if __name__ == "__main__":
 
     #read the .dat file here and fill the arrays
-    #default = [(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10),(11,11)]
+    default = [(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10),(11,11)]
     #test1 = [(0,1),(1,0),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10),(11,11)]
-    a = QuadraticAssignment("1.dat")
+    a = QuadraticAssignment("1.dat", 15000) #core dumps after 15000 bc python
     #print("{0} \n {1} \n {2}".format(a.N,a.D,a.W))
     #print(a.placement)
     #print(a.tabu_list)
@@ -87,3 +133,8 @@ if __name__ == "__main__":
     #print(a.calculate_fitness(a.N, a.D, a.W, default))
     #print(a.calculate_fitness(a.N, a.D, a.W, test1))
     #print(a.partial_sum(a.N, a.D, a.W, [(0,0), (1,1)]))
+    print(a.placement, a.calculate_fitness(a.N, a.D, a.W, a.placement))
+    #print(default, a.calculate_fitness(a.N, a.D, a.W, default))
+    result = a.tabu_search(a.N, a.D, a.W, a.iteration, 1, a.tabu_list, a.placement)
+    #result = a.tabu_search(a.N, a.D, a.W, a.iteration, 0.5, a.tabu_list, default)
+    print(result, a.calculate_fitness(a.N, a.D, a.W, result))
