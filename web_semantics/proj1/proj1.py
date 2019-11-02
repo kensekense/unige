550114d9cded52739a9afe7754cc8bd011a24522
Kense <ningkense@alumni.wfu.edu>
2019-11-02 12:23:41 +0900

    prelim proj1 ups

---
+++ web_semantics/proj1/proj1.py
@@ -0,0 +1,8 @@
+#Ning and Zhang
+#01 November 2019
+#The Korea-China Special
+
+from bs4 import BeautifulSoup
+
+#need to update the filepath
+soup = BeautifulSoup(filepath, 'xml') #soupify the XML data
